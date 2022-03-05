4d7dbeaf27aea8dcb47e1028c3833b20b5c2e414
renshoek <58260544+renshoek@users.noreply.github.com>
2022-03-05 21:07:53 +0100

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,34 @@
+function submit1() {
+  var p = document.getElementById("submit1");
+  p.style.display = "block";  	 
+}
+
+function submit2() {
+  var p = document.getElementById("submit2");
+  p.style.display = "block";  	 
+}
+
+function submit3() {
+  var p = document.getElementById("submit3");
+  p.style.display = "block";  	 
+}
+
+function submit4() {
+  var p = document.getElementById("submit4");
+  p.style.display = "block";  	 
+}
+
+function submit5() {
+  var p = document.getElementById("submit5");
+  p.style.display = "block";  	 
+}
+
+function submit6() {
+  var p = document.getElementById("submit6");
+  p.style.display = "block";  	 
+}
+
+function myFunction() {
+  var x = document.getElementById("button1");
+  x.style.display = "none"; //just let the css value of button to none
+}
